00594bc5092d9daf11a5158adb6f7ddfba234efa
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2023-08-04 10:19:09 +0800

    added html to contact.js

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -4,7 +4,37 @@ import React from 'react';
 function Contact() {
   return (
     <section id="contact">
-      {/* Add your contact form here */}
+      <h2>Contact Me</h2>
+      {formSubmitted ? (
+        <p>Thank you for your message! I'll get back to you soon.</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </section>
   );
 }
