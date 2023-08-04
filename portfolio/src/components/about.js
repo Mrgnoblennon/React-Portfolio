73b4e9273d9c9210d31392026cbb762ac426dff3
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2023-08-04 10:01:27 +0800

    added html to about.js

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -4,7 +4,21 @@ import React from 'react';
 function About() {
   return (
     <section id="about">
-      {/* Add your photo and bio here */}
+      <div className="about-container">
+        <div className="about-photo">
+          <img src={myPhoto} alt="My Photo" />
+        </div>
+        <div className="about-bio">
+          <h2>About Me</h2>
+          <p>
+            Hi, I'm Chase. I'm a passionate web developer with experience
+            in building interactive and user-friendly websites. I enjoy working
+            with React and creating engaging user interfaces. When I'm not
+            coding, you can find me exploring new technologies or hiking in the
+            great outdoors.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
