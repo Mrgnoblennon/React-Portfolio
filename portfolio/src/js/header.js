62a3586b54a6ba8b233d2382acdfe5f621c80eac
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2023-08-04 09:18:12 +0800

    added header component

---
+++ portfolio/src/js/header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+import Navigation from './Navigation';
+
+function Header() {
+  return (
+    <header>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
