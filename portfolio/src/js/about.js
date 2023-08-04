567c812706e75d89f040c77ad2598b61df4bf9b8
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2023-08-04 09:20:09 +0800

    added about component

---
+++ portfolio/src/js/about.js
@@ -0,0 +1,12 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      {/* Add your photo and bio here */}
+    </section>
+  );
+}
+
+export default About;
