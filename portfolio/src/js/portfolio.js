3b312816ebf4535e58b1eca143f075d32d144a4f
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2023-08-04 09:20:49 +0800

    added portfolio component

---
+++ portfolio/src/js/portfolio.js
@@ -0,0 +1,13 @@
+// src/components/Portfolio.js
+import React from 'react';
+import Project from './Project';
+
+function Portfolio() {
+  return (
+    <section id="portfolio">
+      {/* Use the Project component to display your projects */}
+    </section>
+  );
+}
+
+export default Portfolio;
