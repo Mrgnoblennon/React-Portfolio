7db272c270fe58d4ffc2200a058f44adda8f012a
Mrgnoblennon <chasebarrettbrown@hotmail.com>
2023-08-16 12:32:16 +0800

    added font

--- portfolio/src/components/portfolio.js
+++ portfolio/src/components/portfolio.js
@@ -5,30 +5,30 @@ function Portfolio() {
     <div id="pink">
     <div class="container">
     
-    <h1 class="title has-text-centered my-6 py-6">Check out my Portfolio</h1>
+    <h1 class="title has-text-centered py-6" id="text-white">Check out my Portfolio below!</h1>
 
     <div class="columns">
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://mrgnoblennon.github.io/Code-Refractor/">
           <div class="box box1" ></div>
-          <div class="hover-message">Code Refractor</div>
+          <div class="hover-message is-size-4">Code Refractor</div>
         </a>
       </div>
   
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://mrgnoblennon.github.io/Random-Password-Generator/">
           <div class="box box2" ></div>
-          <div class="hover-message">Password Generator</div>
+          <div class="hover-message is-size-4">Password Generator</div>
         </a>
       </div>
     
 
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://mrgnoblennon.github.io/Daily-Planner/">
           <div class="box box3" ></div>
-          <div class="hover-message">Day Planner</div>
+          <div class="hover-message is-size-4">Day Planner</div>
         </a>
       </div>
     </div>
@@ -36,72 +36,72 @@ function Portfolio() {
 
     <div class="columns">
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://mrgnoblennon.github.io/Weather-Dashboard/">
           <div class="box box4" ></div>
-          <div class="hover-message">Weather Dashboard</div>
+          <div class="hover-message is-size-4">Weather Dashboard</div>
         </a>
       </div>
     
 
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://mrgnoblennon.github.io/Quiz-Time/">
           <div class="box box5" ></div>
-          <div class="hover-message">JavaScript Quiz</div>
+          <div class="hover-message is-size-4">JavaScript Quiz</div>
         </a>
       </div>
     
 
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://github.com/Mrgnoblennon/README-Generator">
           <div class="box box6" ></div>
-          <div class="hover-message">README Generator</div>
+          <div class="hover-message is-size-4">README Generator</div>
         </a>
       </div>
     </div>
 
     <div class="columns">
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://github.com/Mrgnoblennon/Logo-Maker">
           <div class="box box7" ></div>
-          <div class="hover-message">Logo Generator</div>
+          <div class="hover-message is-size-4">Logo Generator</div>
         </a>
       </div>
     
 
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://polar-ridge-61059.herokuapp.com/">
           <div class="box box8" ></div>
-          <div class="hover-message">Note Taker</div>
+          <div class="hover-message is-size-4">Note Taker</div>
         </a>
       </div>
     
 
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://github.com/Mrgnoblennon/Employee-Tracker">
           <div class="box box9" ></div>
-          <div class="hover-message">Employee Tracker</div>
+          <div class="hover-message is-size-4">Employee Tracker</div>
         </a>
       </div>
     </div>
 
     <div class="columns ">
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://paichienfeng.github.io/Mix-Colour-Game/">
           <div class="box box10" ></div>
-          <div class="hover-message">Mix-Colour Game</div>
+          <div class="hover-message is-size-4">Mix-Colour Game</div>
         </a>
       </div>
     
 
     
       <div class="column is-one-third">
-        <a class="box-link" href="https://www.example.com">
+        <a class="box-link" href="https://github.com/Mrgnoblennon/NoSQL-Social-Network-API">
           <div class="box box11" ></div>
-          <div class="hover-message">NoSQL Social Network</div>
+          <div class="hover-message is-size-4">NoSQL Social Network</div>
         </a>
       </div>
     
@@ -110,7 +110,7 @@ function Portfolio() {
       <div class="column is-one-third">
         <a class="box-link" href="https://www.example.com">
           <div class="box box12" ></div>
-          <div class="hover-message">Text-Editor</div>
+          <div class="hover-message is-size-4">Text-Editor</div>
         </a>
       </div>
     </div>
